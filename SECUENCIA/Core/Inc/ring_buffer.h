e838a4f7d9ec80d71efd703c405eae1ba4a2a4f6
RafaelTorresCH <rtorresch@unal.edu.co>
2024-08-22 21:57:02 -0500

    Deteccion secuecias
    se detecta la secuencia prevista, mas sin embargo se debe actualizar porque se daño la lectura

---
+++ SECUENCIA/Core/Inc/ring_buffer.h
@@ -0,0 +1,24 @@
+/*
+ * ring_buffer.h
+ *
+ *  Created on: Aug 15, 2024
+ *      Author: torre
+ */
+
+#ifndef INC_RING_BUFFER_H_
+#define INC_RING_BUFFER_H_
+#include <stdint.h>
+
+
+void ring_buffer_reset(void);//ok
+uint8_t ring_buffer_size(void);//ok
+uint8_t ring_buffer_is_full(void);//ok
+uint8_t ring_buffer_is_empty(void);//ok
+
+uint8_t check_sequence(void);
+
+void ring_buffer_write(uint8_t data);
+uint8_t ring_buffer_read(uint8_t *byte);
+
+
+#endif /* INC_RING_BUFFER_H_ */
---
+++ SECUENCIA/Core/Src/ring_buffer.c
@@ -0,0 +1,132 @@
+/*
+ * ring_buffer.c
+ *
+ *  Created on: Aug 15, 2024
+ *      Author: torre
+ */
+
+#include "ring_buffer.h"
+#define capacity (8)//capacidad del buffe
+uint8_t ring_buffer[capacity];//buffer
+uint8_t head_ptr;//cabeza buffer
+uint8_t tail_ptr;//cola buffer
+uint8_t is_full;
+uint8_t seq_index;
+uint8_t SEQUENCE_SIZE = 10;
+/**
+ * @brief: esta funcion se encarga de escribir un dato en el buffer
+ *
+ * @param data: indica el dato que se va a escribir
+ *
+ * retval ninguno
+ * */
+void ring_buffer_write(uint8_t data) {
+    ring_buffer[head_ptr] = data;
+    head_ptr++;
+
+    if (head_ptr >= capacity) {
+        head_ptr = 0;
+    }
+    //cuando la cabeza es mayor que la capacidad, se resetea la cabeza
+    if (head_ptr == tail_ptr) {
+        is_full = 1;//indica que ya esta lelno el buffer, y que la cola esta lista para avanzar
+    }
+
+    if (is_full != 0) {
+        tail_ptr++;//la cola aumenta
+    }
+    if (tail_ptr >= capacity) {
+        tail_ptr = 0;//reseteo de la cola
+    }
+}
+/**
+ * @brief: esta funcion se encarga de escribir un dato en el buffer
+ *
+ * @param data: indica el dato que se va a escribir
+ *
+ * retval ninguno
+ * */
+uint8_t ring_buffer_read (uint8_t *byte) {
+    if ((is_full != 0) || (head_ptr != tail_ptr)) {
+        *byte = ring_buffer[tail_ptr];
+        tail_ptr++;
+        if (tail_ptr >= capacity) {
+            tail_ptr = 0;
+        }
+        is_full = 0;
+        return 1; // Buffer tenía datos y se leyó un byte
+    }
+    return 0; // Buffer está vacío
+}
+
+
+uint8_t ring_buffer_size(void){
+	if(head_ptr > tail_ptr){
+		return head_ptr - tail_ptr;
+
+	}else{
+
+		return capacity - tail_ptr + head_ptr;// esta formula indica el numero de elemntos presentes en el buffer, ha ocurrido un wrap-around
+	}
+
+}
+void ring_buffer_reset(void){
+	for(uint8_t i = 0; i < 8; i++){
+		ring_buffer[0] = 0;//reset buffer
+	}
+}
+
+uint8_t ring_buffer_is_full(void){
+	if(is_full != 0){
+		return 1;// buffer full
+	}else{
+		return 0; //buffer no full
+	}
+}
+uint8_t ring_buffer_is_empty(void){
+	for(uint8_t i = 0; i < 8; i++){
+			if(ring_buffer[i] != 0){
+				return 1;//buffer no empty
+			}
+	}
+	return 0;
+	//buffer empty
+}
+
+
+uint8_t check_sequence(void) {
+    uint8_t byte;
+    uint8_t sequence[10] =  {'1', '0', '0', '7', '2', '4', '6', '6', '0', '6'};
+    if (ring_buffer_read(&byte)) {
+        if (byte == sequence[seq_index]) {
+            seq_index++;
+            if (seq_index == SEQUENCE_SIZE) {
+                // Secuencia encontrada
+            	return 1;
+                // Aquí puedes realizar la acción deseada cuando la secuencia se encuentra
+                seq_index = 0; // Reiniciar búsqueda para buscar nuevas secuencias
+            }
+        } else {
+            seq_index = 0; // Reiniciar búsqueda si no coincide
+        }
+    }
+    return 0;
+}
+//Implemente la siguente funcionalidad en el programa:
+//Usando la libreria de ring buffer, imprima su nombre en consola cuando
+//reciba por uart su numero de documento completo.
+//
+//uint8_t cc(void){
+//	static uint8_t full;
+//	uint8_t secuence[8] = {1,0,0,7,2,4,6,6,0,6};
+//	if(is_full == 1){
+//		full = 1;
+//	}
+//	if(full == 1){
+//
+//
+//
+//	}
+//	return 1;
+//
+//}
